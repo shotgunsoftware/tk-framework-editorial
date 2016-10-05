5654fd3327c77d4176ebb45c327781ca70f1ab08
KP <kp@shotgunsoftware.com>
2016-10-04 23:15:00 -0500

    Updated license

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,13 +1,9 @@
-# Copyright (c) 2016 Shotgun Software Inc.
+# Copyright 2016 Autodesk, Inc. All rights reserved.
 #
-# CONFIDENTIAL AND PROPRIETARY
+# Use of this software is subject to the terms of the Autodesk license agreement
+# provided at the time of installation or download, or which otherwise accompanies
+# this software in either electronic or hard copy form.
 #
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
-# Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
-# not expressly granted therein are reserved by Shotgun Software Inc.
-
 import os
 import decimal
 import unittest2 as unittest
