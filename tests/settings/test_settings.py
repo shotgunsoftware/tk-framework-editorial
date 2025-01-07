61c2fdd5b3517961a7e5f7957165996e18f2be46
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2025-01-07 10:13:31 -0500

    Update tests for Python 3.11

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -1,12 +1,5 @@
 import os
 import sys
-from mock import Mock
-
-import sgtk
-
-# Mock Qt since we don't have it.
-sgtk.platform.qt.QtCore = Mock()
-sgtk.platform.qt.QtGui = Mock()
 
 repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.insert(0, os.path.join(repo_root, "python"))
